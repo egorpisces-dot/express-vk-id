957f3ae7b7211a85d9ee5266406ba63f774a6645
egorpisces-dot <egorpisces@gmail.com>
2025-11-17 14:29:20 +0500

    Update callback.js

--- api/callback.js
+++ api/callback.js
@@ -1,1 +1,16 @@
-package.json
+// api/callback.js
+module.exports = (req, res) => {
+  try {
+    const code = req.query.code;
+
+    if (code) {
+      res.status(200).send(`<h1>Код авторизации: ${code}</h1>`);
+    } else {
+      res.status(200).send("<h1>Нет кода авторизации</h1>");
+    }
+  } catch (err) {
+    console.error("Ошибка функции:", err);
+    res.status(500).send("<h1>Внутренняя ошибка сервера</h1>");
+  }
+};
+
